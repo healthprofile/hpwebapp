9542eec789aa9315eb5beb24f1f43a9d5e2dff0c
Morsal Niyaz <morsalniyaz@gmail.com>
2019-03-28 20:05:44 -0700

    update dashboard

---
+++ app/components/App/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Login from '../Login/index.jsx';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        Helloooooo
+      </div>
+    )
+  }
+} 
